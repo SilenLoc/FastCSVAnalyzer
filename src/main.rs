a2f4570088acd0731530527f0e35277de90c812f
Silen Locatelli <privat@silenlocatelli.ch>
2022-10-06 15:28:18 +0000

    add new checkAction

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate core;
 
 
+
 use log::info;
 
 use crate::init::init;
@@ -13,8 +14,14 @@ mod reader;
 
 fn main() {
     init();
-    analyze(|str| my_check_action(str), |string| my_out(string))
+    analyze(|str| my_check_action2(str), |string| my_out(string))
 }
+
+
+
+
+
+
 
 fn analyze(predicate: impl Fn(String) -> (bool, String), handle_action: impl Fn(String) -> ()) {
     if let Ok(lines) = read_lines(String::from("src/resources/x.csv")) {
@@ -28,6 +35,24 @@ fn analyze(predicate: impl Fn(String) -> (bool, String), handle_action: impl Fn(
         }
     }
 }
+
+
+
+fn my_check_action2(string: String) -> (bool, String) {
+
+let x: Vec<String> = vec!["someValue".to_string(), "someOther".to_string(), "valueThatShouldNotBeIn".to_string()];
+
+let split = string.split(";");
+let as_string_vec: Vec<&str> = split.into_iter().collect();
+
+    
+let result_vec: Vec<bool> = x.into_iter().map(|value| as_string_vec.contains(&value.as_str())).collect();
+
+let result =  result_vec.into_iter().all(|result_per_string|result_per_string);
+
+(result, string)
+}
+
 
 fn my_check_action(string: String) -> (bool, String) {
     let split = string.split(";");
